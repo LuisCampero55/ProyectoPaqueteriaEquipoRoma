d12c1861fd1f7edf12a6bac0729f0a9c622600e6
AxelRGZ <105399523+AxelRGZ@users.noreply.github.com>
2022-06-28 14:25:43 -0500

    Update Clientes.sql

--- Clientes/Clientes.sql
+++ Clientes/Clientes.sql
@@ -3,7 +3,7 @@ create database if not exists proy3;
 use proy3;
 
 CREATE TABLE `Datos_personales` (
-  `idUsuario` INT NOT NULL,
+  `idUsuario` INT NOT NULL auto_increment,
   `nombre_empresa` VARCHAR(45) NOT NULL,
   `cp` VARCHAR(45) NOT NULL,
   `Correo` VARCHAR(45) NOT NULL,
@@ -11,13 +11,24 @@ CREATE TABLE `Datos_personales` (
   PRIMARY KEY (`idUsuario`));
 
 
-SELECT * FROM idUsuario;
+SELECT * FROM `Datos_personales`;
 
-INSERT INTO `idUsuario` (`idUsuario`, `nombre_empresa`, `cp`, `Correo`, `Contraseña`) VALUES ('U00', 'Walmart', '55300', 'walmart@live.com', '4 cilindros');
-INSERT INTO `idUsuario` (`idUsuario`, `nombre_empresa`, `cp`, `Correo`, `Contraseña`) VALUES ('U01', 'Soriana', '33600', 'soriana@live.com', '6 cilindros');
-INSERT INTO `idUsuario` (`idUsuario`, `nombre_empresa`, `cp`, `Correo`, `Contraseña`) VALUES ('T00', 'Bodega Aurrera', '15588', 'bodega@live.com', '4 cilindros');
-INSERT INTO `idUsuario` (`idUsuario`, `nombre_empresa`, `cp`, `Correo`, `Contraseña`) VALUES ('T01', 'Express', '630200', 'express@live.com', '4 cilindros');
-INSERT INTO `idUsuario` (`idUsuario`, `nombre_empresa`, `cp`, `Correo`, `Contraseña`) VALUES ('T03', 'Waldos', '45000', 'Waldos@live.com', '4 cilindros');
+INSERT INTO `Datos_personales` (`idUsuario`, `nombre_empresa`, `cp`, `Correo`, `Contraseña`) VALUES (0, 'Walmart', '55300', 'walmart@live.com', '4 cilindros');
+INSERT INTO `Datos_personales` (`idUsuario`, `nombre_empresa`, `cp`, `Correo`, `Contraseña`) VALUES (1, 'Soriana', '33600', 'soriana@live.com', '6 cilindros');
+INSERT INTO `Datos_personales` (`idUsuario`, `nombre_empresa`, `cp`, `Correo`, `Contraseña`) VALUES (2, 'Bodega Aurrera', '15588', 'bodega@live.com', '4 cilindros');
+INSERT INTO `Datos_personales` (`idUsuario`, `nombre_empresa`, `cp`, `Correo`, `Contraseña`) VALUES (3, 'Express', '630200', 'express@live.com', '4 cilindros');
+INSERT INTO `Datos_personales` (`idUsuario`, `nombre_empresa`, `cp`, `Correo`, `Contraseña`) VALUES (4, 'Waldos', '45000', 'Waldos@live.com', '4 cilindros');
+insert into Datos_personales( nombre_empresa, cp, Correo, Contraseña) VALUES ('${nombre_empresa}', '${cp}', '${Correo}', '${Contraseña}');
+
+INSERT INTO `Datos_personales` (`nombre_empresa`, `cp`, `Correo`, `Contraseña`) VALUES ('Walmart 2', '55300', 'walmart@live.com', '4 cilindros');
+
+DELETE from Datos_personales where idUsuario = 22;
+
+
+
+
+
+
 
 
 
