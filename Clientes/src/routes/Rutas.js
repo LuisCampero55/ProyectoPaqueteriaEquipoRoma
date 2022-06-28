1b0e97f68c4c71c5544d120d74aa7c225acaceac
AxelRGZ <105399523+AxelRGZ@users.noreply.github.com>
2022-06-28 14:24:29 -0500

    Update Rutas.js

--- Clientes/src/routes/Rutas.js
+++ Clientes/src/routes/Rutas.js
@@ -4,27 +4,52 @@ const conexion = require('../config/conexion');
 
 
 
-ruta.get('/',(req, res)=>{
+ruta.get('/take',(req, res)=>{
+    let sql ='select * from datos_personales'
+    conexion.query(sql,(err, rows, fields)=>{
+        if(err) throw err;
+        else{
+            res.json(rows)
+        }
+    })
 
-    res.send('Wenas')
-});
+})
 
 
-routes.post('/agregar',(req,res)=>{
-    const {idUsuario, nombre_empresa, cp, Correo, Contraseña} = req.body
-    let sql = 'select * from datos_personales'
-    conecta.query(sql,(err, rows, fields)=>{
-        if(!err) res.json(rows)
+
+/*
+ruta.post('/agregar',(req,res)=>{
+    const { nombre_empresa, cp, Correo, Contraseña} = req.body
+    let sql = `insert into Datos_personales(nombre_empresa, cp, Correo, Contraseña) VALUES ('${nombre_empresa}', '${cp}', '${Correo}', '${Contraseña}')`
+    conexion.query(sql,(err, rows, fields)=>{
+        if(!err) throw err
         else {
             res.json({status:'usuario registrado'})
         }
     })
 });
 
-routes.delete('/:id',(req, res)=>{
+ */
+
+
+
+
+ruta.post('/agregar',(req,res)=>{
+    const {idUsuario, nombre_empresa, cp, Correo, Contraseña} = req.body
+    let sql = `insert into Datos_personales( idUsuario, nombre_empresa, cp, Correo, Contraseña) VALUES ('${idUsuario}','${nombre_empresa}', '${cp}', '${Correo}', '${Contraseña}')`
+    conexion.query(sql,(err, rows, fields)=>{
+        if(err) throw err
+        else{
+            res.json({status: 'usuario agregado'})
+        }
+    })
+});
+
+
+ruta.delete('/:id',(req, res)=>{
     const{id} = req.params
-    let sql =`delete from datos_personales where idUsuarios = '${id}'`
-    conecta.query(sql, (err, rows, fields)=>{
+    let sql =`delete from datos_personales where idUsuario = '${id}'`
+    conexion.query(sql, (err, rows, fields)=>{
         if(err) throw err
         else{
             res.json({status: 'Usuario eliminado'})
@@ -33,13 +58,23 @@ routes.delete('/:id',(req, res)=>{
 });
 
 
-
-
-
-
-
-
-
+ruta.put('/:id',(req,res)=>{
+    const {id} = req.params
+    const {idUsuario, nombre_empresa, cp, Correo, Contraseña} = req.body
+    let sql = `update Datos_personales set 
+                idUsuario = '${idUsuario}',
+                nombre_empresa = '${nombre_empresa}',
+                cp = '${cp}',
+                Correo = '${Correo}',
+                Contraseña = '${Contraseña}',
+                where idUsuario = '${id}'`
+    conexion.query(sql,(err, rows, fields)=>{
+        if(!err) throw err
+        else {
+            res.json({status:'Conductor actualizado'})
+        }
+    })
+});
 
 
 module.exports = ruta;
