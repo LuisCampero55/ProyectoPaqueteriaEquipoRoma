bbb3031d804cdf84f41cb46b32317bed37ca5cf5
ghc4884 <67808322+ghc4884@users.noreply.github.com>
2022-07-14 21:47:54 -0500

    Add files via upload

---
+++ Paquetes/frontend/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { InicioComponent } from './Componentes/inicio/inicio.component';
+import { AgregarComponent } from './Componentes/agregar/agregar.component';
+import { ModificarComponent } from './Componentes/modificar/modificar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    InicioComponent,
+    AgregarComponent,
+    ModificarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
